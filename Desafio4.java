c3903bfd22d3ca17e5973ef5117e309376024415
Nathalie Vieira Neves <137420230+nathalievneves@users.noreply.github.com>
2024-09-06 12:43:42 -0300

    Update Desafio4.java

--- Desafio4.java
+++ Desafio4.java
@@ -3,10 +3,10 @@ package desafios2;
 public class Desafio4 {
 	//4) ...
 	
-	    //a) 1, 3, 5, 7, ___ = 9 (n+2)
-		//b) 2, 4, 8, 16, 32, 64, ____ = 128 (n*2)
-		//c) 0, 1, 4, 9, 16, 25, 36, ____ = 49 (n*n, em ordem crescente, ou seja 7*7)
-		//d) 4, 16, 36, 64, ____ = 100 (n*n, em odem crescente mas onde n é par, ou seja 10*10, ) 
-		//e) 1, 1, 2, 3, 5, 8, ____ = 13 (n-1)+(n-2)- Sequência de Fibonacci
+	    //a) 1, 3, 5, 7, ___ = 9 (n+2);
+		//b) 2, 4, 8, 16, 32, 64, ____ = 128 (n*2);
+		//c) 0, 1, 4, 9, 16, 25, 36, ____ = 49 (n*n, em ordem crescente, ou seja 7*7);
+		//d) 4, 16, 36, 64, ____ = 100 (n*n, em ordem crescente mas somente se n é par, ou seja 10*10, ) ;
+		//e) 1, 1, 2, 3, 5, 8, ____ = 13 (n-1)+(n-2), ou seja, a Sequência de Fibonacci;
 		//f) 2,10, 12, 16, 17, 18, 19, ____ = 200 (n começa com a letra D em Português).
 }
