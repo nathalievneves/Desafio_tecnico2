c027120e9e9b06d9bcdc47f4ae40e63b04a18d55
Nathalie Vieira Neves <137420230+nathalievneves@users.noreply.github.com>
2024-09-06 11:10:27 -0300

    Create Desafio3.java

---
+++ Desafio3.java
@@ -0,0 +1,18 @@
+package desafios2;
+
+public class Desafio3 {
+
+
+	public static void main(String[] args) {
+		int INDICE = 12, SOMA = 0, K = 1;
+		
+		while (K < INDICE) {
+			K = K + 1; 
+			SOMA = SOMA + K;
+		}
+		System.out.println(+SOMA);
+	}
+
+}
+
+// A resposta é 77. 
