a3a1a82229a25277e5482b426b7d5f3459b51e48
Nathalie Vieira Neves <137420230+nathalievneves@users.noreply.github.com>
2024-09-06 11:54:23 -0300

    Create Desafio5.java

---
+++ Desafio5.java
@@ -0,0 +1,19 @@
+package desafios2;
+
+public class Desafio5 {
+
+		//5) ...
+		
+		//Primeiro passo:
+		//Ligo o interruptor  1 por 10 minutos e o desligo (para a lâmpada ficar quente).
+		//Segundo passo:
+		//Depois, ligo o interruptor 2 e já vou até a sala 1;
+		//Se a lâmpada estiver acesa, é controlada pelo interruptor 2; se estiver quente pelo interruptor 1
+		// e se estiver desligada pelo interruptor 3;
+		//Terceiro passo:
+		//Deixo desligado o interruptor da resposta que descobrimos da sala 1,e ligo um dos que sobraram;
+		//vou até a sala 2 e vejo se a lâmpada estiver acesa é o interruptor que eu liguei, se não, é o desligado.
+		
+		
+			
+	}
